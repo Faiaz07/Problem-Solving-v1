01d96dd2b75e5d66dc0ca9f4b59c933e3edb3f09
Faiaz Khan Soumik <66191188+Faiaz07@users.noreply.github.com>
2022-12-27 12:59:23 +0600

    Update halfpyramid.java

--- Pattern/halfpyramid.java
+++ Pattern/halfpyramid.java
@@ -12,7 +12,6 @@ public class halfpyramid {
         Scanner sc = new Scanner(System.in);
 
         int r = sc.nextInt();
-//        int c = sc.nextInt();
         for(int i=1; i<=r; i++){
             for(int j=1; j<=i; j++){
                 System.out.print("*");
