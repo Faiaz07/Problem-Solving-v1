c97e62d5599c07454039879ae1bedc340e9a341d
Faiaz Khan Soumik <66191188+Faiaz07@users.noreply.github.com>
2022-12-27 12:51:07 +0600

    Add files via upload

---
+++ Pattern/filledrectangle.java
@@ -0,0 +1,28 @@
+/*
+
+ *****
+ *****
+ *****
+
+ */
+import java.util.*;
+
+public class filledrectangle {
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+
+        int r = sc.nextInt();
+        int c = sc.nextInt();
+
+        for (int i = 1; i <= r; i++) {
+            for (int j = 1; j <= c; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+    }
+}
---
+++ Pattern/halfpyramid.java
@@ -0,0 +1,27 @@
+/*
+
+ *
+ **
+ ***
+ ****
+
+ */
+import java.util.*;
+public class halfpyramid {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int r = sc.nextInt();
+//        int c = sc.nextInt();
+        for(int i=1; i<=r; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+
+
+    }
+}
